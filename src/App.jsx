cffa3593ae1e9435dadfee3807bb74209bee8ae3
Karthik Gowda <karthikgowda.y@gmail.com>
2026-02-18 18:59:47 +0530

    learning-tailwind

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <div className = "flex justify-between">
+        <div className="bg-blue-300">
+          hi there 
+        </div>
+        
+        <div style={{background : "blue"}}>
+          hi there 
+        </div>
+
+        <div style={{background : "green"}}>
+          hi there 
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
